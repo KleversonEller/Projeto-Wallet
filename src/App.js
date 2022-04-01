7d372610578151cda1420147c1100fb2aa89df1c
Kleverson Eller <kleverson.a16@gmail.com>
2022-03-31 21:18:50 -0300

    Requisito - 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import Wallet from './pages/Wallet';
+import Wallet from './pages/Wallet';
 import Login from './pages/Login';
 
 class App extends React.Component {
@@ -8,6 +8,7 @@ class App extends React.Component {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
